ca6559cd446019057496e585a36c7b956a1b048a
AmagiDDmxh <amagiddmxh@gmail.com>
2022-04-24 22:56:07 +0800

    add layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,82 @@
 import ERC20 from '@components/ERC20'
+import { useIsMounted } from '@libs/hooks/useIsMounted'
 import Head from 'next/head'
 import { useAccount, useConnect } from 'wagmi'
 
 export const Home = (): JSX.Element => {
+  const isMounted = useIsMounted()
+
   const [{ data: accountData }, disconnect] = useAccount({
     fetchEns: true,
   })
-  const [{ data: connectData, error: connectError }, connect] = useConnect()
+  const [
+    { data: connectData, error: connectError, loading: loadingConenctor },
+    connect,
+  ] = useConnect()
 
   return (
-    <div>
+    <div className="flex flex-col w-full h-screen">
       <Head>
         <title>Create Next App with Amagi:DDmxh</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
+      <header className="flex justify-around items-center">
+        <div className="bg-red-200 p-4">LOGO</div>
+
+        <nav className="">
+          <a href="#link-a" target="_blank" rel="noopener noreferrer">
+            link a
+          </a>
+          <a href="#link-a" target="_blank" rel="noopener noreferrer">
+            link a
+          </a>
+          <a href="#link-a" target="_blank" rel="noopener noreferrer">
+            link a
+          </a>
+          <a href="#link-a" target="_blank" rel="noopener noreferrer">
+            link a
+          </a>
+        </nav>
+
         {accountData ? (
-          <div className="text-3xl text-black">
+          <div className="text-black">
             {/* TODO */}
             <img src={accountData.ens?.avatar ?? ''} alt="ENS Avatar" />
-            <div>
+            <div className="text-2xl">
               {accountData.ens?.name
                 ? `${accountData.ens?.name} (${accountData.address})`
                 : accountData.address}
             </div>
             {/* TODO */}
-            <div>Connected to {accountData?.connector?.name}</div>
-            <button onClick={disconnect}>Disconnect</button>
+            <div className="text-sm">
+              Connected to {accountData?.connector?.name}
+            </div>
+            <button
+              className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={disconnect}
+            >
+              Disconnect
+            </button>
           </div>
         ) : (
           <div className="flex gap-2 p-4">
             {/* // TODO */}
             {connectData.connectors.map((connector) => (
               <button
-                disabled={!connector.ready}
+                disabled={isMounted && !connector.ready}
                 key={connector.id}
-                onClick={() => connect(connector)}
+                onClick={() => {
+                  connect(connector)
+                }}
               >
-                {connector.name}
-                {!connector.ready && ' (unsupported)'}
+                {connector.id === 'injected'
+                  ? isMounted
+                    ? connector.name
+                    : connector.id
+                  : connector.name}
+                {isMounted && !connector.ready && ' (unsupported)'}
+                {loadingConenctor && connector.name === connector?.name && '…'}
               </button>
             ))}
             {connectError && (
@@ -49,7 +86,7 @@ export const Home = (): JSX.Element => {
         )}
       </header>
 
-      <main className="border-2 mt-2">
+      <main className="h-full bg-slate-700">
         <ERC20 />
       </main>
     </div>
